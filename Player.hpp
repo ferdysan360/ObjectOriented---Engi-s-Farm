f131593c4985a545a3130de9b05bbd2562086156
Ferdy Santoso <36350020+ferdysan360@users.noreply.github.com>
2019-03-10 08:42:27 +0700

    Create Player.hpp

---
+++ Player.hpp
@@ -0,0 +1,29 @@
+// class player
+// file : player.hpp
+
+#ifndef _PLAYER_HPP_
+#define _PLAYER_HPP_
+#define MAX_WATER 100
+#define MAX_BAG 10
+#include "Product.hpp"
+
+class Player {
+    private:
+        int WaterContainer;         // Container air player, nilai maksimumnya adalah MAX_WATER
+
+        Product* Bag;               // Tas player, nilai maksimumnya adalah MAX_BAG
+
+    public:
+        Player();                   // ctor player, inisialisasi waterContainer = 0 & bag = 0
+
+        Player(int, Product*);      // ctor user-defined player, inisialisasi
+
+        Player(const Player&);      // cctor player
+
+        Player& operator=(const Player&); // operator= overloading class player
+
+
+
+};
+
+#endif
