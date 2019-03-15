c1604244ce7b565caab62845e162d479029103a6
Andrian Cedric <42271632+anakduaribu@users.noreply.github.com>
2019-03-15 15:56:17 +0700

    Add files via upload

--- Facility.hpp
+++ Facility.hpp
@@ -8,10 +8,10 @@
 
 class Facility : public Cell{
     private:
-    
+
     public:
-        Well(); //Untuk mengisi WaterContainer
-        Mixer(); //Combine 2 atau lebih bahan-bahan untuk menghasilkan SideProduct
-        Truck(); //Menjual seluruh isi tas, Truck dipakai dlm selang waktu tertentu
+        void Well(); //Untuk mengisi WaterContainer
+        void Mixer(); //Combine 2 atau lebih bahan-bahan untuk menghasilkan SideProduct
+        void Truck(); //Menjual seluruh isi tas, Truck dipakai dlm selang waktu tertentu
 };
 #endif
